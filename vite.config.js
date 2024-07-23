1af41a30c7be489d63aebd97820d2c5ed7504e05
striker0234 <98385735+striker0234@users.noreply.github.com>
2024-07-24 03:54:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
